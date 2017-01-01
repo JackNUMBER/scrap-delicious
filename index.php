49b3ed1830ef5f92b671d9d45b1c7d6eb0c96fc1
JackNUMBER <antoine.cdrt@gmail.com>
2017-01-01 17:57:16 +0100

    refacto js

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
             <h3 class="panel-title">Scrap Delicious</h3>
         </div>
         <div class="panel-body form-inline">
-            <form method="get" class="js-form-fetch" data-fetch-type="page" data-fetch-page="1">
+            <form method="get" class="js-form-fetch">
                 <fieldset>
                     <div class="form-group">
                         <label for="username">Username</label>
@@ -32,50 +32,56 @@
                     </div>
                 </fieldset>
             </form>
-            <button class="btn js-stoploop">Stop</button>
         </div>
     </div>
 
     <div class="alert alert-danger js-error-panel hidden" role="alert"></div>
-    <div class="result-ajax"></div>
     <div class="result-ajax">
         <!-- Scrapping -->
-        <div class="js-state-scrap text-center">
+        <div class="js-state-scrap hidden text-center">
             <div class="h4">
-                <span class="glyphicon glyphicon-repeat loader"></span> Scrapping page <span class="js-current-page">x</span> / <span class="js-total-page">x</span>
+                <span class="glyphicon glyphicon-repeat loader"></span> Scrapping page <span class="js-current-page">1</span> / <span class="js-total-page">x</span>
+            </div>
+            <div>
+                <button class="btn js-stoploop">Stop process</button><br>
+                <small class="js-stop-process-label hidden">Finsishing the current task</small>
+
             </div>
         </div>
         <!-- Success -->
-        <div class="js-state-success text-center">
+        <div class="js-state-success hidden text-center">
             <div class="h4">
                 <span class="glyphicon glyphicon-ok text-success"></span> Finish
             </div>
-            <div class="js-error-message">
-                <span class="js-bookmarks-scrapped">x</span> bookmarks scrapped
+            <div>
+                <span class="js-bookmarks-scrapped">x</span> bookmarks scrapped.
+            </div>
+            <div>
+                You can download your bookmarks:
             </div>
         </div>
         <!-- Error -->
-        <div class="js-state-error text-center">
+        <div class="js-state-error hidden text-center">
             <div class="h4">
                 <span class="glyphicon glyphicon-remove text-danger"></span> Error
             </div>
-            <div>
+            <div class="js-error-message">
                 An error occured.
             </div>
         </div>
         <!-- Stopped -->
-        <div class="js-state-stop text-center">
+        <div class="js-state-stop hidden text-center">
             <div class="h4">
                 <span class="glyphicon glyphicon-indent-left text-warning"></span> Interrupted
             </div>
             <div>You have stopped the process but you can still download the <span class="js-bookmarks-scrapped">x</span> scrapped bookmarks:</div>
         </div>
         <!-- Buttons -->
-        <div class="text-center download-buttons js-download-buttons">
+        <div class="download-buttons js-state-download js-download-buttons hidden text-center">
             <button class="btn btn-primary js-btn-download" data-fetch-type="html1" rel="tooltip" data-placement="top" title="Firefox, Pocket">HTML Type 1</button>
             <button class="btn btn-primary js-btn-download hidden" data-fetch-type="html2" rel="tooltip" data-placement="top" title="">HTML Type 2</button>
             <button class="btn btn-primary js-btn-download hidden" data-fetch-type="json" rel="tooltip" data-placement="top" title="">JSON</button>
-            <br><a href="https://github.com/JackNUMBER/scrap-delicious"><small>Need more help?</a></small>
+            <br><a href="https://github.com/JackNUMBER/scrap-delicious"><small>Need help?</a></small>
         </div>
     </div>
 
@@ -83,10 +89,9 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script>
 $(function () {
-    $("[rel='tooltip']").tooltip();
+    $('[rel="tooltip"]').tooltip();
 
     var fetch_form = $('.js-form-fetch'),
-        error_panel = $('.js-error-panel'),
         loader = '<span class="glyphicon glyphicon-repeat loader"></span>',
         total_pages,
         scrapped_bookmarks = 0,
@@ -95,7 +100,6 @@ $(function () {
 
     function fetchPage(username, page){
         var current_page = page ? page : 1;
-        error_panel.addClass('hidden');
 
         $.ajax({
             url: 'ajax.php?username=' + username + '&page=' + current_page,
@@ -106,13 +110,52 @@ $(function () {
             },
             error: function(result) {
                 console.log('error', result);
+                loop_state = 'error';
                 fetchError(result);
             }
         });
     }
 
+    function fetchSuccess(result) {
+        if (result.message == 'ok') {
+            if (loop_state == 'initial') {
+                total_pages = result.total_pages;
+                $('.js-total-page').text(total_pages);
+            }
+
+            scrapped_bookmarks += result.bookmarks.length;
+            $('.js-bookmarks-scrapped').text(scrapped_bookmarks);
+
+            if (loop_state != 'stop') {
+                if (result.page >= total_pages) {
+                    // end
+                    loop_state = 'finish';
+
+                } else {
+                    // continue
+                    loop_state = 'scrap';
+                    $('.js-current-page').text(parseInt(result.page) + 1);
+                    fetchPage(user, parseInt(result.page) + 1);
+                }
+            }
+
+            updateDisplay(result);
+        } else if (result.message == 'error') {
+            loop_state = 'error';
+            fetchError(result);
+        }
+    }
+
+    function fetchError(result) {
+        if (result.errors) {
+            $('.js-error-message').html(result.errors.join('<br>'));
+        }
+
+        updateDisplay(result);
+    }
+
     function toggleAbleForm() {
-        console.log('toggleAbleForm()', fetch_form.find('fieldset'));
+        console.log('toggleAbleForm()');
         if (fetch_form.find('fieldset').attr('disabled')) {
             fetch_form.find('fieldset').removeAttr('disabled');
             fetch_form.find('input').removeAttr('disabled');
@@ -124,62 +167,35 @@ $(function () {
         }
     }
 
-    function fetchSuccess(result) {
-        if (result.page >= total_pages) {
-            loop_state = false;
-            $('.result-ajax').html('<span class="glyphicon glyphicon-ok text-success"></span> Finish').append('<br><span class="js-bookmarks-scrapped">x</span> bookmarks scrapped');
-            toggleAbleForm();
-            updateData(result);
-        } else if (result.message == 'ok'){
-            updateData(result);
-        } else if (result.message == 'error') {
-            fetchError(result);
-            $('.result-ajax').html('');
-        }
-
-        if (loop_state) {
-            $('.result-ajax').html(loader + ' Scrapping page <span class="current-scrap">' + (parseInt(result.page) + 1) +'</span> / ' + total_pages);
-            fetchPage(user, parseInt(result.page) + 1);
-        } else {
-            console.log('loop_state', loop_state);
-            toggleAbleForm();
-            // $('.result-ajax').html('<span class="glyphicon glyphicon-indent-left"></span> Interrupted');
-        }
-    }
-
-    function fetchError(result) {
-        loop_state = false;
-        error_panel.html(result.errors.join('<br>'));
-        error_panel.removeClass('hidden');
-    }
-
-    function updateData(result) {
-        if (result.page == 1) {
-            total_pages = result.total_pages;
-        }
-        scrapped_bookmarks += result.bookmarks.length
-        $('.js-bookmarks-scrapped').text(scrapped_bookmarks);
-    }
-
     function stopLoop() {
-        console.log('set to False');
-        loop_state = false;
+        loop_state = 'stop';
+        $('.js-stop-process-label').removeClass('hidden');
     }
 
     function updateDisplay(result) {
+        $('[class*="js-state-"').addClass('hidden');
         switch(loop_state) {
             case false:
                 break;
             case 'initial':
+                $('.js-state-scrap').removeClass('hidden');
                 break;
             case 'scrap':
+                $('.js-state-scrap').removeClass('hidden');
                 break;
             case 'finish':
+                $('.js-state-success').removeClass('hidden');
+                $('.js-download-buttons').removeClass('hidden');
+                toggleAbleForm();
                 break;
             case 'stop':
-                break;
+                $('.js-state-stop').removeClass('hidden');
+                $('.js-download-buttons').removeClass('hidden');
+                toggleAbleForm();
                 break;
             case 'error':
+                $('.js-state-error').removeClass('hidden');
+                toggleAbleForm();
                 break;
             default:
         }
@@ -190,12 +206,17 @@ $(function () {
 
         user = $(this).find('#username').val();
 
-        loop_state = true;
+        loop_state = 'initial';
         toggleAbleForm();
 
-        fetchPage(user);
+        // init scrap
+        scrapped_bookmarks = 0;
+        $('.js-current-page').text('1');
+        $('.js-total-page').text('x');
+        $('.js-stop-process-label').addClass('hidden');
 
-        $('.result-ajax').html(loader + ' Scrapping page <span class="current-scrap">1</span> / x').addClass('text-center h4');
+        updateDisplay();
+        fetchPage(user);
     });
 
     $('.js-stoploop').on('click', function() {
